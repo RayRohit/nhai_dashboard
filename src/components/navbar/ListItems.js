74195482582166badad2531c0132094b2ca304ea
Rohit Ray <rohitray.kurma@navajna.com>
2022-12-05 02:27:12 +0530

    updated list items

--- src/components/navbar/ListItems.js
+++ src/components/navbar/ListItems.js
@@ -27,6 +27,8 @@ import { Link, NavLink } from "react-router-dom";
 export const MainListItems = ({ open }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [copen, setCopen] = useState(false);
+  const [dopen, setDopen] = useState(false);
+
   const [anchorEl, setAnchorEl] = useState(null);
   const Menuopen = Boolean(anchorEl);
 
@@ -45,6 +47,9 @@ export const MainListItems = ({ open }) => {
   const handleClick = () => {
     setCopen(!copen);
   };
+  const handleClickk = () => {
+    setDopen(!dopen);
+  };
 
   return (
     <div className="listContainer">
@@ -62,7 +67,7 @@ export const MainListItems = ({ open }) => {
               onClick={handleClick}
             >
               <img className="sidebar-icons" src={Contacts} alt="Contacts" />
-            Operations & Maintenance 
+              Operations & Maintenance 
             </NavLink>
             <Collapse in={copen} timeout="auto" unmountOnExit>
               <List className="listgroup" component="div" disablePadding>
@@ -78,9 +83,9 @@ export const MainListItems = ({ open }) => {
                 <NavLink to="highway" className="collapseStyle">
                   Highway
                 </NavLink>
-                <NavLink to="highwayprogress" className="collapseStyle">
+                {/* <NavLink to="highwayprogress" className="collapseStyle">
                   Highway Progress
-                </NavLink>
+                </NavLink> */}
               </List>
             </Collapse>
           </div>
@@ -123,25 +128,60 @@ export const MainListItems = ({ open }) => {
                   Highway
                 </MenuItem>
               </Link>
-              <Link to="highwayprogress" className="link_style text-black">
+              {/* <Link to="highwayprogress" className="link_style text-black">
                 <MenuItem onClick={(event) => handleMenuItemClick(event)}>
                   Highway Progress
-                </MenuItem>
-              </Link>
-
-              {/* <Link to="highway-safety" className="link_style text-black">
-                <MenuItem onClick={(event) => handleMenuItemClick(event)}>
-                  Highway
-                </MenuItem>
-              </Link>
-              <Link to="user-safety" className="link_style text-black">
-                <MenuItem onClick={(event) => handleMenuItemClick(event)}>
-                  Highway
                 </MenuItem>
               </Link> */}
             </Menu>
           </div>
         )}
+        {open ? (
+          <div>
+            <NavLink
+              to="highwayprogress"
+              className="text-white sidebarStyle"
+              onClick={handleClickk}
+            >
+              <img className="sidebar-icons" src={Contacts} alt="Contacts" />
+              Operation & Construction 
+            </NavLink>
+            <Collapse in={dopen} timeout="auto" unmountOnExit>
+              <List className="listgroup" component="div" disablePadding>
+                <NavLink to="highwayprogress" className="collapseStyle">
+                  Highway Progress
+                </NavLink>
+              </List>
+            </Collapse>
+          </div>
+        ) : (
+          <div>
+            <ListItemButton onClick={handleClickListItem}>
+              <ListItemIcon>
+                <img className="sidebar-icons" src={Contacts} alt="Contacts" />
+              </ListItemIcon>
+              <ListItemText primary="Contacts" />
+            </ListItemButton>
+            <Menu
+              id="lock-menu"
+              anchorEl={anchorEl}
+              open={Menuopen}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "lock-button",
+                role: "listbox",
+              }}
+              sx={{ ml: 7, mt: -6 }}
+            >
+              <Link to="highwayprogress" className="link_style text-black">
+                <MenuItem onClick={(event) => handleMenuItemClick(event)}>
+                  Highway Progress
+                </MenuItem>
+              </Link>
+            </Menu>
+          </div>
+        )}
+        
         {/* <NavLink to="mycardpage" className="text-white sidebarStyle">
                     <img className="sidebar-icons" src={MyCard} alt="MyCard" />
                     My Card
